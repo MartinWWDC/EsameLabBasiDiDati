1a813d120fe1cc7ef9ef0e7f0235036ec398aee4
Billy <martinoleone5@gmail.com>
2023-06-03 00:00:17 +0200

    add_voto

---
+++ sql/add_voto.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION add_voto(votoP integer, id_studenteP varchar, id_corsoP integer, "dataP" timestamp)
+RETURNS void AS $$
+BEGIN
+	UPDATE sostiene 
+	SET voto = votoP
+	WHERE id_studente = id_studenteP AND id_corso = id_corsoP AND "data" = "dataP";
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT add_voto(30, '123456', 1, '2023-05-30 10:00:00');
