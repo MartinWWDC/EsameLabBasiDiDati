ffc6af6f2f6c2fad8de061b54e151bfda2392603
Billy <martinoleone5@gmail.com>
2023-06-21 20:42:45 +0200

    Update get_carriera_valida_archvio.sql

--- sql/get_carriera_valida_archvio.sql
+++ sql/get_carriera_valida_archvio.sql
@@ -1,12 +1,12 @@
-create or replace function get_carriera_valida_archivio(id_studente varchar)
-returns table(id_insegnamento integer, nome_insegamento varchar,voto integer) as $$
+CREATE OR REPLACE FUNCTION get_carriera_valida_archivio(id_studente varchar)
+RETURNS TABLE(id_insegnamento integer, nome_insegnamento varchar, voto voto) AS $$
 BEGIN
-	return QUERY
-
-	select i.id_insegnamento, i.nome_insegamento,v.voto
-	from insegnamento i 
-	--todo 
-	return;
+	RETURN QUERY
+	SELECT i.id, i."nomeInsegnamento", v.voto
+	FROM insegnamento i
+	JOIN insegnamento_arc i_arc ON i.id = i_arc.id_insegnamento
+	JOIN voti_arc v ON i_arc.id_voto=v.id
+	WHERE v.studente = id_studente;
 END;
-
-$$ language plpgsql;
+$$ LANGUAGE plpgsql;
+select * from get_carriera_valida_archivio('34')
