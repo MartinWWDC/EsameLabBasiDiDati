579b00c028885b9a4e64d26e74a274eaa1a16d63
Billy <60852345+MartinWWDC@users.noreply.github.com>
2023-08-06 17:04:19 +0200

    logic archivio implemented

---
+++ sql/student_arch.sql
@@ -0,0 +1,61 @@
+
+
+
+CREATE OR REPLACE FUNCTION calcola_periodo_inattivita(studente_id varchar)
+RETURNS INTERVAL AS
+$$
+DECLARE
+    data_sostenimento TIMESTAMP;
+BEGIN
+    -- Ottieni la data di sostenimento dell'esame più antico per lo studente
+    SELECT g.data_sostenimento INTO data_sostenimento
+    FROM get_carriera(studente_id) g
+    ORDER BY g.data_sostenimento ASC
+    LIMIT 1;
+
+    IF data_sostenimento IS NULL THEN
+        RETURN NULL;
+    END IF;
+    
+
+    RETURN CURRENT_TIMESTAMP - data_sostenimento;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION archivia_studente_tr()
+RETURNS TRIGGER AS
+$$
+DECLARE
+    periodo_inattivita INTERVAL;
+    record_voto RECORD;
+    voto_id INTEGER;
+BEGIN
+    periodo_inattivita := calcola_periodo_inattivita(OLD.matricola);
+    
+    INSERT INTO studente_arc VALUES (OLD.matricola, OLD.email,OLD.pass, OLD.nome, OLD.cognome,OLD.cfu, OLD.cellulare, periodo_inattivita,OLD."idLaurea");
+
+    FOR record_voto IN SELECT * FROM get_carriera_valida(OLD.matricola)
+    LOOP
+  
+        INSERT INTO voti_arc (id,voto,"dataEsame",studente)
+        VALUES (DEFAULT, record_voto.voto, record_voto.data_sostenimento, OLD.matricola) RETURNING id INTO voto_id;
+        INSERT INTO insegnamento_arc(id_voto,id_insegnamento) VALUES (voto_id,record_voto.id_insegnamento);
+        DELETE FROM Sostiene WHERE id_studente=OLD.matricola;
+    END LOOP;
+    
+    RETURN OLD;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER before_delete_studenti
+BEFORE DELETE ON "Studente"
+FOR EACH ROW
+EXECUTE FUNCTION archivia_studente_tr();
+
+
+DELETE from "Studente" where matricola='123456'
