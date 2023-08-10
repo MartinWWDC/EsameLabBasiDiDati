c3767fe0b2df8a5ab39d2ec2de0f9e7336013760
Billy <60852345+MartinWWDC@users.noreply.github.com>
2023-08-10 18:52:31 +0200

    createAppello implemented

--- web/docente/appello/creaAppello.php
+++ web/docente/appello/creaAppello.php
@@ -18,11 +18,11 @@
     require  '../../public_components/utility.php';
     session_start();
     $sql = 'select * from insegnamento where responsabile=$1';
-    $id=$_SESSION['user'][0];
-    
+    $id = $_SESSION['user'][0];
+
     $params = array($id);
     $result = launchSQL($sql, $params, "get_insegnamenti");
-    
+
 
 
     ?>
@@ -35,46 +35,45 @@
                 </div>
                 <div class="card bg-light">
                     <article class="card-body mx-auto" style="max-width: 800px;">
-                        <form>
+                        <form action='create.php' id='appelloForm' method="POST">
                             <div class="form-group input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"> <i class="fa fa-building"></i> </span>
                                 </div>
-                                <select class="form-control">
-                                    <?php 
+                                <select class="form-control" name="corso">
+                                    <?php
                                     while ($row = pg_fetch_row($result)) {
                                         //echo var_dump($row);
-                                        echo '<option>'.$row[1].'</option>';
-
+                                        echo '<option value="' . $row[0] . '">' . $row[1] . '</option>';
                                     }
-                                    
+
                                     ?>
-                                    
+
                                 </select>
                             </div>
                             <div class="form-group input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"> <i class="fa fa-user"></i> </span>
                                 </div>
-                                <input name="" class="form-control" placeholder="Luogo" type="text">
+                                <input name="luogo" class="form-control" placeholder="Luogo" type="text">
                             </div>
                             <div class="form-group input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"> <i class="fa fa-envelope">Data</i> </span>
                                 </div>
 
-                                <input name="" class="form-control" placeholder="Data e ora " type="Date">
+                                <input name="dataA" class="form-control" placeholder="Data e ora" type="Date" onchange="formatDate()">
                             </div>
                             <div class="form-group input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"> <i class="fa fa-envelope">Ora</i> </span>
                                 </div>
-                                
-                                <input name="" class="form-control" placeholder="Data e ora " type="time">
+
+                                <input name="oraA" class="form-control" placeholder="Data e ora " type="time">
                             </div>
 
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block"> Crea Appello </button>
+                                <button type="submit" value="sub" class="btn btn-primary btn-block"> Crea Appello </button>
                             </div>
                         </form>
                     </article>
@@ -85,6 +84,37 @@
         </div>
     </div>
 </body>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+        const form = document.getElementById('appelloForm');
+        form.addEventListener('submit', function(event) {
+            let currentDate = `${day}-${month}-${year}`;
+            if (!form.corso.value || !form.luogo.value || !form.dataA.value || !form.oraA.value) {
+                event.preventDefault(); // Impedisce l'invio del modulo se i campi non sono completi
+                alert('Completa tutti i campi prima di inviare il modulo.');
+            }
+            if(form.dataA.value<=currentDate){
+                alert('Non può creare un appello per un giorno passato');
+            }
+        });
+    });
+
+    function formatDate() {
+        var input = document.getElementById("myDateInput");
+        var inputValue = input.value; // Formato: "yyyy-mm-dd"
+
+        var parts = inputValue.split("-");
+        var formattedDate = parts[2] + "/" + parts[1] + "/" + parts[0];
+
+        input.value = formattedDate;
+    }
+
+</script>
 <!-- Includi i file JavaScript di Bootstrap -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
---
+++ web/docente/appello/create.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Includi i file CSS di Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+
+<body>
+    <?php include '../../public_components/checkD.php' ?>
+    <?php include '../../public_components/header.php' ?>
+    <div class="container-fluid">
+        <div class="row">
+           
+            <?php include '../components/sidebar.php'; ?>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+            <?php
+            require  '../../public_components/utility.php';
+            session_start();
+            $corso = $_POST['corso'] . "\n";
+            $place = $_POST['luogo'] . "\n";
+            $day = $_POST['dataA'] . "\n";
+            $time = $_POST['oraA'] . ':00';
+            $dataA = $day . ' ' . $time;
+            $params = array($dataA, $place, $corso);
+            $sql = 'INSERT INTO appello ("dataA", "luogo", "corso") VALUES ($1, $2, $3)';
+            $result = launchSQL($sql, $params, "get_insegnamenti");
+
+            ?>
+            </main>
+        </div>
+    </div>
+
+</body>
+<!-- Includi i file JavaScript di Bootstrap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</html>
