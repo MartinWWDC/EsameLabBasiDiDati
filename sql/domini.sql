409c3a88e9a3190f537f49829a33dcccd4f93364
Billy <60852345+MartinWWDC@users.noreply.github.com>
2023-08-11 23:47:05 +0200

    fix domain  vote

--- sql/domini.sql
+++ sql/domini.sql
@@ -1,6 +1,6 @@
 --domini 
 CREATE DOMAIN voto AS integer
-   CONSTRAINT valid_voto CHECK (VALUE >= 1 AND VALUE <= 30);
+   CONSTRAINT valid_voto CHECK (VALUE >= 0 AND VALUE <= 30);
 CREATE DOMAIN annoDiInsengnamento AS integer
    CONSTRAINT valid_annoDiInsengnamento CHECK (VALUE >= 1 AND VALUE <= 3);
 
