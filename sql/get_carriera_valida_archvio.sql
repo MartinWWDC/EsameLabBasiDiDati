286738ce6c20f330790effde3dad79026e0b42f9
Billy <60852345+MartinWWDC@users.noreply.github.com>
2023-08-19 16:07:02 +0200

    SQL: fix get_carriera_archvio  return table

--- sql/get_carriera_valida_archvio.sql
+++ sql/get_carriera_valida_archvio.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION get_carriera_valida_archivio(id_studente varchar)
-RETURNS TABLE(id_insegnamento integer, nome_insegnamento varchar, voto voto) AS $$
+RETURNS TABLE(id_insegnamento integer, nome_insegnamento varchar, voto voto, dataC date) AS $$
 BEGIN
 	RETURN QUERY
-	SELECT i.id, i."nomeInsegnamento", v.voto
+	SELECT i.id, i."nomeInsegnamento", v.voto,v."dataEsame"
 	FROM insegnamento i
 	JOIN insegnamento_arc i_arc ON i.id = i_arc.id_insegnamento
 	JOIN voti_arc v ON i_arc.id_voto=v.id
